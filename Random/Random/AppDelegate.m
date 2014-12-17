bc2c5732844200a7d1bf9d4ff658566d10d9534d
Palaniappan Vairavan <palani.vairavan@gmail.com>
2014-12-16 22:34:46 -0800

    Initial check-in

---
+++ Random/Random/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Random
+//
+//  Created by Palaniappan Vairavan on 12/15/14.
+//  Copyright (c) 2014 Sivasoft. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ Random/Random/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Random
+//
+//  Created by Palaniappan Vairavan on 12/15/14.
+//  Copyright (c) 2014 Sivasoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
