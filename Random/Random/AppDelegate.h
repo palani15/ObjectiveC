8221cefbf4237841261771f7ac8bbfda40880d0a
Palaniappan Vairavan <palani.vairavan@gmail.com>
2014-12-16 23:55:42 -0800

    Initial check in

---
+++ Random/Random/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Random
+//
+//  Created by Palaniappan Vairavan on 12/16/14.
+//  Copyright (c) 2014 Sivasoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
